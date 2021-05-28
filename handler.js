666479168683797ed76e0473fe7749c7562639c1
Pasquale Panuccio <pasquale.panuccio@xpeppers.com>
2021-05-28 17:33:06 +0200

    Added converter function

--- handler.js
+++ handler.js
@@ -2,7 +2,8 @@
 
 const AWS = require('aws-sdk');
 const axios = require('axios');
-const sharp = require('sharp')
+const sharp = require('sharp');
+const xml2js = require('xml2js');
 
 const S3 = new AWS.S3()
 
@@ -72,6 +73,34 @@ module.exports.resizer = async (event, context) => {
   }
 };
 
+module.exports.converter = async (event, context) => {
+
+  const { outputRoute, outputToken, inputS3Url } = event.getObjectContext || {};
+
+  const { data: xml } = await axios.get(inputS3Url, { responseType: 'application/xml' })
+
+  // convert XML to JSON
+  xml2js.parseString(xml, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    // `result` is a JavaScript object
+    // convert it to a JSON string
+    const json = JSON.stringify(result, null, 4);
+
+  });
+
+  await S3.writeGetObjectResponse({
+    RequestRoute: outputRoute,
+    RequestToken: outputToken,
+    Body: json
+  }).promise()
+
+  return {
+    statusCode: 200
+  }
+};
+
 module.exports.hello = async event => {
   return {
     statusCode: 200,
@@ -84,7 +113,4 @@ module.exports.hello = async event => {
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
--- test/test-object-lambda-script.py
+++ test/test-object-lambda-script.py
@@ -2,12 +2,14 @@ import boto3
 
 s3 = boto3.client('s3')
 
-print('Request Image resized by S3 Object Lambda:')
-image = s3.get_object(
-    Bucket='arn:aws:s3-object-lambda:REGION:ACCOUNT:accesspoint/BUCKET-AP',
-    Key='richmond_720x480.jpg')["Body"]
-with open('pictures/richmond_720x480.jpg', 'wb') as f:
-    for i in image._raw_stream:
-        f.write(i)
-        f.close
-print('Downloaded with Success')
+print('Original object from the S3 bucket:')
+original = s3.get_object(
+    Bucket='BUCKET',
+    Key='user.xml')
+print(original['Body'].read().decode('utf-8'))
+
+print('Object processed by S3 Object Lambda:')
+transformed = s3.get_object(
+    Bucket='arn:aws:s3-object-lambda:eu-west-1:123412341234:accesspoint/myolap',
+    Key='user.xml')
+print(transformed['Body'].read().decode('utf-8'))
